a2cbddabc70cff54e4047fbc545e05f87246a30a
Md Yeasin <mdyeasinhn@gmail.com>
2024-06-15 21:10:58 +0600

    footer updated

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,12 +2,12 @@
 
 const Footer = () => {
     return (
-        // <footer className="text-center space-y-2 bg-gray-400 w-full pb-11 mt-10">
-        //     <h2 className="font-semibold pt-12">Tour X Pro</h2>
-        //     <h2 className="font-semibold ">Providing reliable Tour X Pro services since 1992</h2>
-        //     <p >Copyright © 2024 - All right reserved</p>
+        <footer className="text-center space-y-2 bg-gray-400 w-full pb-11 mt-10">
+            <h2 className="font-semibold pt-12">Tour X Pro</h2>
+            <h2 className="font-semibold ">Providing reliable Tour X Pro services since 1992</h2>
+            <p >Copyright © 2024 - All right reserved</p>
 
-        // </footer>
+        </footer>
     );
 };
 
